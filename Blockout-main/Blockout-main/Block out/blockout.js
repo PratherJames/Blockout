28264b2a664cc688737ed6a66230e9b69a806f38
PratherJames <116032695+PratherJames@users.noreply.github.com>
2022-10-25 20:14:20 -1000

    Add files via upload

---
+++ Blockout-main/Blockout-main/Block out/blockout.js
@@ -0,0 +1,9 @@
+ function display() {
+            document.getElementById("video").style.filter = "brightness(0%)";
+ /*           document.getElementById("screen").style.filter = "contrast(50%)";*/
+         }
+ function bright() {
+            document.getElementById("video").style.filter = "brightness(100%)";
+ /*           document.getElementById("screen").style.filter = "contrast(100%)";*/
+         }
+console.log("it worked");
